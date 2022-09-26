2a3eeb6ff27bad281099097ac700b47057a7fbad
saiyajin17 <106419562+saiyajin17@users.noreply.github.com>
2022-09-27 00:39:43 +0530

    Added State in class

--- src/componets/Newsitem.js
+++ src/componets/Newsitem.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
   
   render() {
-    let {title,desc} = this.props;
+    let {title,desc,imageUrl,newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/7777/production/_126838503_gettyimages-1426954270.jpg" className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{desc}</p>
